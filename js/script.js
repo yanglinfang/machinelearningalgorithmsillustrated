1eea20f45d3d68d332de8ce051e5465e69ae123b
Lin <yanglinfang@ischool.berkeley.edu>
2016-11-29 18:45:15 -0800

    allow fast_rewind

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   // $.ajax();  <<< CORE METHOD
   // $('').load();
@@ -8,7 +8,7 @@ $(function(){
   // $.getJSON();
   callPage('#home')
 
- $(document.body).on('click', '.page' , function(e){
+  $(document.body).on('click', '.page', function (e) {
     e.preventDefault();
     var pageRef = $(this).find('a').attr('href');
     callPage(pageRef)
@@ -16,74 +16,84 @@ $(function(){
   });
 
   var scatterLR = [
-    {x1:98,x2:5.5,y:0},
-    {x1:120,x2:5.8,y:0},
-    {x1:168,x2:6.2,y:0},
-    {x1:200,x2:5.5,y:1},
-    {x1:210,x2:5,y:1},
-    {x1:168,x2:5,y:1}
+    { x1: 98, x2: 5.5, y: 0 },
+    { x1: 120, x2: 5.8, y: 0 },
+    { x1: 168, x2: 6.2, y: 0 },
+    { x1: 200, x2: 5.5, y: 1 },
+    { x1: 210, x2: 5, y: 1 },
+    { x1: 168, x2: 5, y: 1 }
   ]
   var scatterNN = [
-    {x1:98,x2:5.5,y:0},
-    {x1:155,x2:5.2,y:0},
-    {x1:120,x2:5.7,y:0},
-    {x1:168,x2:6.2,y:0},
-    {x1:200,x2:5.5,y:1},
-    {x1:210,x2:5,y:1},
-    {x1:168,x2:5,y:1},
-    {x1:110,x2:6.0,y:1},
-    {x1:125,x2:5.8,y:1}
+    { x1: 98, x2: 5.5, y: 0 },
+    { x1: 155, x2: 5.2, y: 0 },
+    { x1: 120, x2: 5.7, y: 0 },
+    { x1: 168, x2: 6.2, y: 0 },
+    { x1: 200, x2: 5.5, y: 1 },
+    { x1: 210, x2: 5, y: 1 },
+    { x1: 168, x2: 5, y: 1 },
+    { x1: 110, x2: 6.0, y: 1 },
+    { x1: 125, x2: 5.8, y: 1 }
   ]
 
-  function setupClickPlay(elem){
-    $(document.body).on('click', "#play"+elem, function(e){
-                 
-                 //first step is normalize data
-                  switch(elem){
-                    case "LR":
-                      normalizeData(scatterLR);
-                      scatter('ScatterLR',scatterLR);
-                      break;
-                    case "NN":
-                      normalizeData(scatterNN);
-                      scatter('ScatterNN',scatterNN);
-                      break;
-                  }
+  function setupClickPlay(elem) {
+    $(document.body).on('click', "#play" + elem, function (e) {
+      //user clicked play
+      //first step is normalize data
+      switch (elem) {
+        case "LR":
+          scatter('ScatterLR', normalizeData(scatterLR));
+          break;
+        case "NN":
+          scatter('ScatterNN', normalizeData(scatterNN));
+          break;
+      }
+    })
 
-                })
+    $(document.body).on('click', "#back" + elem, function (e) {
+      //user clicked back
+      //setback data
+      switch (elem) {
+        case "LR":
+          scatter('ScatterLR', scatterLR);
+          break;
+        case "NN":
+          scatter('ScatterNN', scatterNN);
+          break;
+      }
+    })
   }
 
-  function callPage(pageRefInput){
+  function callPage(pageRefInput) {
     var pageName = pageRefInput.split("#");
-    pageName = pageName[1]+".html";
+    pageName = pageName[1] + ".html";
 
     $.ajax({
-        url: pageName,
-        type: "GET",
-        dataType : 'text',
-        success: function( response ) {
-          $('#content').html(response);
-          scatter('ScatterLR',scatterLR);
-          scatter('ScatterNN',scatterNN);
-          draw('LR',[2,1]);
-          draw('NN',[2,3,1]);
-          drawLossChart('LRLoss');
-          drawLossChart('NNLoss');
-          setupClickPlay('LR');
-          setupClickPlay('NN');
-          setTimeout(function(){
-              MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-            }, 1);
-        },
-        error: function( error ) {
-          console.log('Error loading page', error);
-        },
-        complete: function( xhr, status ) {
-          console.log("Request complete");
-        }
-    });    
+      url: pageName,
+      type: "GET",
+      dataType: 'text',
+      success: function (response) {
+        $('#content').html(response);
+        scatter('ScatterLR', scatterLR);
+        scatter('ScatterNN', scatterNN);
+        draw('LR', [2, 1]);
+        draw('NN', [2, 3, 1]);
+        drawLossChart('LRLoss');
+        drawLossChart('NNLoss');
+        setupClickPlay('LR');
+        setupClickPlay('NN');
+        setTimeout(function () {
+          MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        }, 1);
+      },
+      error: function (error) {
+        console.log('Error loading page', error);
+      },
+      complete: function (xhr, status) {
+        console.log("Request complete");
+      }
+    });
   }
-  
+
 
 
 });
--- js/simple-scatter.js
+++ js/simple-scatter.js
@@ -1,168 +1,174 @@
 
 
 
-function getParam(paramName, data){
-	switch(paramName){
+function getParam(paramName, data) {
+	switch (paramName) {
 		case 'x1_min':
-		    return d3.min(data, function(d) { return d["x1"]; });
+			return d3.min(data, function (d) { return d["x1"]; });
 		case 'x1_max':
-			return d3.max(data, function(d) { return d["x1"]; });
+			return d3.max(data, function (d) { return d["x1"]; });
 		case 'x1_span':
-			return d3.max(data, function(d) { return d["x1"]; }) - d3.min(data, function(d) { return d["x1"]; });
+			return d3.max(data, function (d) { return d["x1"]; }) - d3.min(data, function (d) { return d["x1"]; });
 		case 'x2_min':
-			return d3.min(data, function(d) { return d["x2"]; });
+			return d3.min(data, function (d) { return d["x2"]; });
 		case 'x2_max':
-			return d3.max(data, function(d) { return d["x2"]; });			
+			return d3.max(data, function (d) { return d["x2"]; });
 		case 'x2_span':
-			return d3.max(data, function(d) { return d["x2"]; }) - d3.min(data, function(d) { return d["x2"]; });
+			return d3.max(data, function (d) { return d["x2"]; }) - d3.min(data, function (d) { return d["x2"]; });
 	}
 }
 
 
-function normalizeData(data){  
+function normalizeData(data) {
 
-    var x1_min = getParam('x1_min', data)
+	var x1_min = getParam('x1_min', data)
 	var x1_span = getParam('x1_span', data)
 	var x2_min = getParam('x2_min', data)
-	var x2_span =getParam('x2_span', data)
+	var x2_span = getParam('x2_span', data)
 
-    data.forEach(function(element) {
-        element.x1 = (element.x1-x1_min)/x1_span;
-        element.x2 = (element.x2-x2_min)/x2_span;
-    }, this);
-    
+	var normalized = [];
+	var i = 0;
+	data.forEach(function (element) {
+		var e = {};
+		e.x1 = (element.x1 - x1_min) / x1_span;
+		e.x2 = (element.x2 - x2_min) / x2_span;
+		e.y = element.y;
+		normalized[i] = e;
+		i++;
+	}, this);
+	return normalized;
 }
 
 
-function scatter(id,data){
+function scatter(id, data) {
 
 	//clean up existing plot
-    d3.select('#'+id + ' svg').remove()
+	d3.select('#' + id + ' svg').remove()
 
-	var margin = {top: 23, right: 30, bottom: 40, left: 40}
-		,width  = 235
-	    ,height = 240
-	
+	var margin = { top: 23, right: 30, bottom: 40, left: 40 }
+		, width = 235
+		, height = 240
+
 
 	var x = d3.scaleLinear()
-	              .domain([getParam('x1_min', data)-getParam('x1_span', data)/7.0, getParam('x1_max', data)+getParam('x1_span', data)/7.0])
-	              .range([ 0, width]);
-	    
+		.domain([getParam('x1_min', data) - getParam('x1_span', data) / 7.0, getParam('x1_max', data) + getParam('x1_span', data) / 7.0])
+		.range([0, width]);
+
 	var y = d3.scaleLinear()
-		      .domain([getParam('x2_min', data)-getParam('x2_span', data)/10.0, getParam('x2_max', data)+getParam('x2_span', data)/10.0])
-		      .range([ height, 0 ]);
+		      .domain([getParam('x2_min', data) - getParam('x2_span', data) / 10.0, getParam('x2_max', data) + getParam('x2_span', data) / 10.0])
+		      .range([height, 0]);
 
 
 	// gridlines in x axis function
-	function make_x_gridlines() {		
-	    return d3.axisBottom(x)
-	        .ticks(10)
+	function make_x_gridlines() {
+		return d3.axisBottom(x)
+			.ticks(10)
 	}
 
 	// gridlines in y axis function
-	function make_y_gridlines() {		
-	    return d3.axisLeft(y)
-	        .ticks(5)
+	function make_y_gridlines() {
+		return d3.axisLeft(y)
+			.ticks(5)
 	}
 
-	var chart = d3.select('#'+id)
+	var chart = d3.select('#' + id)
 		.append('svg:svg')
 		.attr('width', width + margin.right + margin.left)
 		.attr('height', height + margin.top + margin.bottom)
 		.attr('class', 'chart')
 
 
-		// add the X gridlines
-		chart.append("g")			
-		  .attr("class", "grid")
-		  .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
-		  .call(make_x_gridlines()
-		      .tickSize(-height)
-		      .tickFormat("")
-		  )
+	// add the X gridlines
+	chart.append("g")
+		.attr("class", "grid")
+		.attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+		.call(make_x_gridlines()
+			.tickSize(-height)
+			.tickFormat("")
+		)
 
-		// add the Y gridlines
-		chart.append("g")			
-		  .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")")
-		  .attr("class", "grid")
-		  .call(make_y_gridlines()
-		      .tickSize(-width)
-		      .tickFormat("")
-		  )
+	// add the Y gridlines
+	chart.append("g")
+		.attr("transform", "translate(" + margin.left + "," + (margin.top) + ")")
+		.attr("class", "grid")
+		.call(make_y_gridlines()
+			.tickSize(-width)
+			.tickFormat("")
+		)
 
-		// add the X Axis
-		chart.append("g")
-		  .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
-		  .call(d3.axisBottom(x)
-		  	// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
-		  	// .tickFormat(d3.format(",.0f"))
-		  	)
+	// add the X Axis
+	chart.append("g")
+		.attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+		.call(d3.axisBottom(x)
+		// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
+		// .tickFormat(d3.format(",.0f"))
+		)
 
-		// add the Y Axis
-		chart.append("g")
-		  .attr("transform", "translate(" + margin.left + "," + (margin.top) + ")")
-		  .call(d3.axisLeft(y)
-		  	// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
-		  	// .tickFormat(d3.format(",.0f"))
-		  	);
-		
+	// add the Y Axis
+	chart.append("g")
+		.attr("transform", "translate(" + margin.left + "," + (margin.top) + ")")
+		.call(d3.axisLeft(y)
+		// .tickValues([0, 1, 2, 3, 4, 5, 6, 7])
+		// .tickFormat(d3.format(",.0f"))
+		);
 
-		var main = chart.append('g')
-			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-			.attr('width', width)
-			.attr('height', height)
-			.attr('class', 'main')
 
-		var g = main.append("svg:g"); 
-	    
-	    g.selectAll("scatter-dots")
-	      .data(data)
-	      .enter().append("svg:circle")
-	      	  .attr("class",function (d){return "data-point-"+d["y"]})
-	          .attr("cx", function (d,i) { return x(d["x1"]); } )
-	          .attr("cy", function (d) { return y(d["x2"]); } )
-	          .attr("r", 6)
+	var main = chart.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+		.attr('width', width)
+		.attr('height', height)
+		.attr('class', 'main')
 
-	      .on("mouseenter",function(d,i){
-	      	console.log("d",d)
-	      	var xPosition = parseFloat(d3.select(this).attr("cx"))-10;
-            var yPosition = parseFloat(d3.select(this).attr("cy"))-50;
+	var g = main.append("svg:g");
 
-            //Update the tooltip position and value
-            var tooltip = d3.select('#'+id).selectAll(".tooltip")
-              .style("left", xPosition + "px")
-              .style("top", yPosition + "px")
-              
-            tooltip.selectAll(".x2")
-              .text("$x_{i2} = "+d['x2']+"\\ ft$")
-              
-            tooltip.selectAll(".x1")
-              .text("$x_{i1} = "+d['x1']+"\\ lbs$")
+	g.selectAll("scatter-dots")
+		.data(data)
+		.enter().append("svg:circle")
+		.attr("class", function (d) { return "data-point-" + d["y"] })
+		.attr("cx", function (d, i) { return x(d["x1"]); })
+		.attr("cy", function (d) { return y(d["x2"]); })
+		.attr("r", 6)
 
-            tooltip.selectAll(".y")
-              .text("$y = "+d['y']+"$")
+		.on("mouseenter", function (d, i) {
+			console.log("d", d)
+			var xPosition = parseFloat(d3.select(this).attr("cx")) - 10;
+			var yPosition = parseFloat(d3.select(this).attr("cy")) - 50;
 
-			setTimeout(function(){
-				MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+			//Update the tooltip position and value
+			var tooltip = d3.select('#' + id).selectAll(".tooltip")
+				.style("left", xPosition + "px")
+				.style("top", yPosition + "px")
+
+			tooltip.selectAll(".x2")
+				.text("$x_{i2} = " + d['x2'] + "\\ ft$")
+
+			tooltip.selectAll(".x1")
+				.text("$x_{i1} = " + d['x1'] + "\\ lbs$")
+
+			tooltip.selectAll(".y")
+				.text("$y = " + d['y'] + "$")
+
+			setTimeout(function () {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 			}, 1);
-			    //Show the tooltip
-			    d3.select('#'+id).selectAll(".tooltip").classed("hidden", false);
+			//Show the tooltip
+			d3.select('#' + id).selectAll(".tooltip").classed("hidden", false);
 
-	      }).on("mouseout", function(d) {       
-               d3.select('#'+id).selectAll(".tooltip").classed("hidden",true);   
-               }); 
+		}).on("mouseout", function (d) {
+			d3.select('#' + id).selectAll(".tooltip").classed("hidden", true);
+		});
 
-	      // now add titles to the axes
-        main.append("text")
-        	.attr("class","mathy")
-            .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-            .attr("transform", "translate("+ (-margin.left*.75) +","+(height/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
-            .text("Height in ft");
+	// now add titles to the axes
+	main.append("text")
+		.attr("class", "mathy")
+		.attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+		.attr("transform", "translate(" + (-margin.left * .75) + "," + (height / 2) + ")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+		.text("Height in ft");
 
-        main.append("text")
-        	.attr("class","mathy")
-            .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
-            .attr("transform", "translate("+ (width/2) +","+(height+(margin.bottom*.95))+")")  // centre below axis
-            .text("Weight in lbs");
+	main.append("text")
+		.attr("class", "mathy")
+		.attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+		.attr("transform", "translate(" + (width / 2) + "," + (height + (margin.bottom * .95)) + ")")  // centre below axis
+		.text("Weight in lbs");
 }
 

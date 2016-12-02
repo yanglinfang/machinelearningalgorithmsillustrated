08b92dc8d6258f4a291f07b537fdf22fa8abb008
junluo-code <junluo168@outlook.com>
2016-12-02 17:26:21 -0500

    adjust loss func plot

--- js/loss.js
+++ js/loss.js
@@ -10,11 +10,11 @@ function drawLossChart(elem) {
     switch(elem){
         case "LR":
             //filePath = url + "/data/LRlosses.txt";
-            filePath= url + "/machinelearningalgorithmsillustrated/data/LRlosses.txt";
+            filePath= url + "/data/LRlosses.txt";
             break;
         case "NN":
             //filePath = url + "/data/NNlosses.txt";
-            filePath= url + "/machinelearningalgorithmsillustrated/data/non-linearly-separable-case/NNlosses.txt";
+            filePath= url + "/data/non-linearly-separable-case/NNlosses.txt";
             break;
     } 
 
